3914174793aa4397e1ff6330c52a6446d9a341c1
Dana <kovach.dana@yandex.ru>
2020-06-25 22:44:26 +0300

    rewrite slider.js to es5

--- js/slider.js
+++ js/slider.js
@@ -1,48 +1,50 @@
-const slider = document.querySelector(`.slider`);
+"use strict";
 
-const sliderRightList = slider.querySelector(`.slider__list`);
+var slider = document.querySelector('.slider');
 
-const sliderToggleLeft = slider.querySelector(`.slider__toggle--left`);
-const sliderToggleRight = slider.querySelector(`.slider__toggle--right`);
+var sliderRightList = slider.querySelector('.slider__list');
 
-const DESKTOP_WIDTH = 1140;
+var sliderToggleLeft = slider.querySelector('.slider__toggle--left');
+var sliderToggleRight = slider.querySelector('.slider__toggle--right');
 
-const RIGHT_SLIDE_ACTIVE_CLASS = `slider__list-item--active`;
-const SLIDE_CONTENT_ACTIVE_CLASS = `slider__content--active`;
+var DESKTOP_WIDTH = 1140;
 
-const SlidesContentId = {
-    slide1: `slideContent1`,
-    slide2: `slideContent2`,
-    slide3: `slideContent3`,
+var RIGHT_SLIDE_ACTIVE_CLASS = 'slider__list-item--active';
+var SLIDE_CONTENT_ACTIVE_CLASS = 'slider__content--active';
+
+var SlidesContentId = {
+    slide1: 'slideContent1',
+    slide2: 'slideContent2',
+    slide3: 'slideContent3',
 };
 
-const SlidesBackground = {
-    slide1: `url("img/slider-background.jpg")`,
-    slide2: `url("img/demo-right-desktop.jpg")`,
-    slide3: `url("img/demo-left-desktop.jpg")`,
+var SlidesBackground = {
+    slide1: 'url("img/slider-background.jpg")',
+    slide2: 'url("img/demo-right-desktop.jpg")',
+    slide3: 'url("img/demo-left-desktop.jpg")',
 };
 
-const clearActiveClass = (items, activeClass) => {
-    items.forEach((item) => {
+var clearActiveClass = function(items, activeClass) {
+    items.forEach(function(item) {
         if (item.classList.contains(activeClass)) {
             item.classList.remove(activeClass);
         }
     });
 };
 
-const setActiveClassForContent = (id) => {
-    const neededItem = document.getElementById(id);
+var setActiveClassForContent = function(id) {
+    var neededItem = document.getElementById(id);
 
     neededItem.classList.add(SLIDE_CONTENT_ACTIVE_CLASS);
 };
 
-const setActiveBackground = (background) => {
+var setActiveBackground = function(background) {
     slider.style.backgroundImage = background;
 };
 
-const setActiveSlide = () => {
-    const sliderActiveItem = slider.querySelector(`.slider__list-item--active`);
-    const sliderContentItems = slider.querySelectorAll(`.slider__content`);
+var setActiveSlide = function() {
+    var sliderActiveItem = slider.querySelector('.slider__list-item--active');
+    var sliderContentItems = slider.querySelectorAll('.slider__content');
 
     clearActiveClass(sliderContentItems, SLIDE_CONTENT_ACTIVE_CLASS);
 
@@ -50,17 +52,19 @@ const setActiveSlide = () => {
     setActiveBackground(SlidesBackground[sliderActiveItem.id]);
 };
 
-const initSwipeSliderForMobile = () => {
-    const CONTENT_SLIDE_SLICE = `Content`;
-    const SWIPE_SLIDER_CLASS = `swipe`;
-    const SWIPE_WRAP_CLASS = `swipe-wrap`;
+var initSwipeSliderForMobile = function() {
+    var CONTENT_SLIDE_SLICE = 'Content';
+    var SWIPE_SLIDER_CLASS = 'swipe';
+    var SWIPE_WRAP_CLASS = 'swipe-wrap';
 
-    const sliderForMobile = document.querySelector(`.slider__left`);
-    const sliderForMobileContent = document.querySelector(`.slider__left-wrapper`);
+    var sliderForMobile = document.querySelector('.slider__left');
+    var sliderForMobileContent = document.querySelector('.slider__left-wrapper');
 
-    const sliderContents = document.querySelectorAll(`.slider__content`);
+    var sliderContents = document.querySelectorAll('.slider__content');
 
-    sliderContents.forEach((item) => item.style.display = `block`);
+    sliderContents.forEach(function(item) {
+        item.style.display = 'block'
+    });
 
     sliderForMobile.classList.add(SWIPE_SLIDER_CLASS);
     sliderForMobileContent.classList.add(SWIPE_WRAP_CLASS);
@@ -72,24 +76,24 @@ const initSwipeSliderForMobile = () => {
         continuous: true,
         disableScroll: true,
         stopPropagation: true,
-        callback: (index, element) => {
-            const idByActiveSlide = element.id;
-            const pureIdByActiveSlide = idByActiveSlide.replace(CONTENT_SLIDE_SLICE, ``);
+        callback: function(index, element) {
+            var idByActiveSlide = element.id;
+            var pureIdByActiveSlide = idByActiveSlide.replace(CONTENT_SLIDE_SLICE, '');
 
             setActiveBackground(SlidesBackground[pureIdByActiveSlide]);
         },
     });
 };
 
-const moveSlideRight = (evt) => {
+var moveSlideRight = function(evt) {
     evt.preventDefault();
 
-    const firstChild = sliderRightList.children[0];
-    const sliderRightItems = slider.querySelectorAll(`.slider__list-item`);
+    var firstChild = sliderRightList.children[0];
+    var sliderRightItems = slider.querySelectorAll('.slider__list-item');
 
     clearActiveClass(sliderRightItems, RIGHT_SLIDE_ACTIVE_CLASS);
 
-    const cloneItem = firstChild.cloneNode(true);
+    var cloneItem = firstChild.cloneNode(true);
 
     sliderRightList.appendChild(cloneItem);
     remove(firstChild);
@@ -99,17 +103,17 @@ const moveSlideRight = (evt) => {
     setActiveSlide();
 };
 
-const moveSlideLeft = (evt) => {
+var moveSlideLeft = function(evt) {
     evt.preventDefault();
 
-    const slidesAmount = sliderRightList.children.length - 1;
+    var slidesAmount = sliderRightList.children.length - 1;
 
-    const lastChild = sliderRightList.children[slidesAmount];
-    const sliderRightItems = slider.querySelectorAll(`.slider__list-item`);
+    var lastChild = sliderRightList.children[slidesAmount];
+    var sliderRightItems = slider.querySelectorAll('.slider__list-item');
 
     clearActiveClass(sliderRightItems, RIGHT_SLIDE_ACTIVE_CLASS);
 
-    const cloneItem = lastChild.cloneNode(true);
+    var cloneItem = lastChild.cloneNode(true);
 
     sliderRightList.prepend(cloneItem);
     remove(lastChild);
@@ -123,5 +127,5 @@ if (window.innerWidth < DESKTOP_WIDTH) {
     initSwipeSliderForMobile();
 }
 
-sliderToggleRight.addEventListener(`click`, moveSlideRight);
-sliderToggleLeft.addEventListener(`click`, moveSlideLeft);
+sliderToggleRight.addEventListener('click', moveSlideRight);
+sliderToggleLeft.addEventListener('click', moveSlideLeft);
