d147d9b5911e8b74fcda88ea70a7c5a5a9ed8087
Dana <kovach.dana@yandex.ru>
2020-06-25 22:23:16 +0300

    rewrite backend.js to es5

--- js/backend.js
+++ js/backend.js
@@ -1,5 +1,7 @@
-const getActionUrl = () => {
-    const form = document.querySelector(`.form`);
+'use strict';
+
+var getActionUrl = function() {
+    var form = document.querySelector('.form');
 
     if (form === null) {
         return;
@@ -8,23 +10,23 @@ const getActionUrl = () => {
     return form.action;
 };
 
-const getMethodForm = () => {
-    const form = document.querySelector(`.form`);
+var getMethodForm = function() {
+    var form = document.querySelector('.form');
 
     return form === null ? null : form.method;
 };
 
-const Status = {
+var Status = {
     OK: 200,
 };
 
-const TIMEOUT = 10000;
+var TIMEOUT = 10000;
 
-const createRequest = (onSuccess, onError) => {
-    const xhr = new XMLHttpRequest();
-    xhr.responseText = `json`;
+var createRequest = function(onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
-    xhr.addEventListener('load', function () {
+    xhr.addEventListener('load', function() {
         if (xhr.status === Status.OK) {
             onSuccess(xhr.response);
         } else {
@@ -32,11 +34,11 @@ const createRequest = (onSuccess, onError) => {
         }
     });
 
-    xhr.addEventListener('error', function () {
+    xhr.addEventListener('error', function() {
         onError('Произошла ошибка соединения');
     });
 
-    xhr.addEventListener('timeout', function () {
+    xhr.addEventListener('timeout', function() {
         onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
 
@@ -45,8 +47,8 @@ const createRequest = (onSuccess, onError) => {
     return xhr;
 };
 
-const send = function (data, onSuccess, onError) {
-    const request = createRequest(onSuccess, onError);
+var send = function(data, onSuccess, onError) {
+    var request = createRequest(onSuccess, onError);
 
     request.open(getMethodForm(), getActionUrl());
     request.send(data);
