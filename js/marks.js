f16a93a3d9ca19d3ae8d6ca9a6c36d45bc25c908
Dana <kovach.dana@yandex.ru>
2020-06-25 22:10:24 +0300

    rewrite marks.js to es5

--- js/marks.js
+++ js/marks.js
@@ -1,43 +1,45 @@
-const demo = document.querySelector(`.demo`);
-const demoButtonActive = demo.querySelector(`.demo__button--active`);
-const demoDetails = demo.querySelector(`.demo__detail`);
-const demoCloseButton = demoDetails.querySelector(`.demo__close`);
+"use strict";
 
-const DEMO_SHOW_CLASS = `demo__detail--show`;
+var demo = document.querySelector(".demo");
+var demoButtonActive = demo.querySelector(".demo__button--active");
+var demoDetails = demo.querySelector(".demo__detail");
+var demoCloseButton = demoDetails.querySelector(".demo__close");
 
-const FADE_IN_CLASS = `fade-in`;
+var DEMO_SHOW_CLASS = "demo__detail--show";
 
-const showDemoDetails = () => {
-    demoDetails.classList.add(FADE_IN_CLASS);
-    demoDetails.classList.add(DEMO_SHOW_CLASS);
+var FADE_IN_CLASS = "fade-in";
 
-    document.addEventListener(`click`, onNoDetailsClick);
-};
-
-const hideDemoDetails = () => {
-    if (demoDetails.classList.contains(DEMO_SHOW_CLASS)) {
-        demoDetails.classList.remove(DEMO_SHOW_CLASS);
-    }
-
-    document.removeEventListener(`click`, onNoDetailsClick);
-};
-
-const onDemoCloseButtonClick = () => {
-    fadeOut(demoDetails, 500, hideDemoDetails);
-};
-
-demoButtonActive.addEventListener(`click`, (evt) => {
-    evt.preventDefault();
-
-    showDemoDetails();
-});
-
-demoCloseButton.addEventListener(`click`, onDemoCloseButtonClick);
-
-const onNoDetailsClick = (evt) => {
+function onNoDetailsClick(evt) {
     if (evt.target === demoButtonActive || evt.target === demoDetails || demoDetails.contains(evt.target)) {
         return;
     }
 
     onDemoCloseButtonClick();
-};
+}
+
+function showDemoDetails() {
+    demoDetails.classList.add(FADE_IN_CLASS);
+    demoDetails.classList.add(DEMO_SHOW_CLASS);
+
+    document.addEventListener("click", onNoDetailsClick);
+}
+
+function hideDemoDetails() {
+    if (demoDetails.classList.contains(DEMO_SHOW_CLASS)) {
+        demoDetails.classList.remove(DEMO_SHOW_CLASS);
+    }
+
+    document.removeEventListener("click", onNoDetailsClick);
+}
+
+function onDemoCloseButtonClick() {
+    fadeOut(demoDetails, 500, hideDemoDetails);
+}
+
+demoButtonActive.addEventListener("click", function(evt) {
+    evt.preventDefault();
+
+    showDemoDetails();
+});
+
+demoCloseButton.addEventListener("click", onDemoCloseButtonClick);
